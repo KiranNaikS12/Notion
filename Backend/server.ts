db1cc3101563c92f90a581a74fba72fe13117526
KiranNaikS12 <kiransnaik56@gmail.com>
2025-03-15 15:50:17 +0530

    fix: Fixed the module_not_found issue for configDB

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import connectDB from 'config/connectDB';
+import connectDB from './config/connectDB';
 import cors from 'cors';
 import userRoutes from './Routes/userRoutes';
 
